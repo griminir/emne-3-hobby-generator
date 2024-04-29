1938851ea721cfc202af9f646b48fedd5e352794
griminir <viktor.elliot.king@gmail.com>
2024-04-29 14:36:01 +0200

    Add project files.

---
+++ emne-3-hobby-generator/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hobby Generator");
+
+string[] hobby = ["chess", "programming", "MMA", "looking at grass grow"];
+Random num = new Random();
+
+
+Console.WriteLine("Who needs a new hobby?");
+var name = Console.ReadLine();
+giveHobby();
+
+
+void giveHobby()
+{
+    int randomHobby = num.Next(hobby.Length);
+    Console.WriteLine($"{name} your new hobby is {hobby[randomHobby]}");
+}
+
+//another way
+Console.WriteLine("Who needs a new hobby?");
+var name2 = Console.ReadLine();
+getHobby();
+
+void getHobby()
+{
+    int randomHobby2 = num.Next(hobby.Length);
+    if (randomHobby2 == 0)
+    {
+        Console.WriteLine($"{name2} your new hobby is watching paint dry");
+    }
+    else if (randomHobby2 == 1)
+    {
+        Console.WriteLine($"{name2} your new hobby having fun");
+    } 
+    else if (randomHobby2 == 2)
+    {
+        Console.WriteLine($"{name2} your new hobby is gaming");
+    }
+    else
+    {
+        Console.WriteLine($"{name2} you dont get a new hobby");
+    }
+}
